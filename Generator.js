6b092127aa55e3de365df50571baa691190953e1
Kyrehx <smugleaf125@gmail.com>
2015-05-31 14:06:36 -0700

    First Commit - all basic html and js completed

---
+++ Generator.js
@@ -0,0 +1,12 @@
+var names = [];
+function addName() {
+    "use strict";
+    var n = document.getElementById("userText").value;
+    document.getElementById("userText").value = "";
+    names.push(n);
+}
+function getOne() {
+    "use strict";
+    var i = Math.floor(Math.random() * names.length);
+    window.alert(names[i]);
+}
